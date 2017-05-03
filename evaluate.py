2d05785befb98be83f34302dba2f38efe0104f53
Benjamin Pikus <bpikus1@ugradx.cs.jhu.edu>
2017-05-03 19:40:11 -0400

    Changed BIC again

--- evaluate.py
+++ evaluate.py
@@ -5,8 +5,12 @@ from train_model import *
 def BIC_eval(Y, F, S, d = 10, h = 5):
     n = len(S)
     k = (n-1) * h
-    sigma = calc_std(Y)
-    mle = (1/sigma)**2 * ss_err(Y, F, S) + (np.log(2 * np.pi) * len(F) * len(S))
+    sigma = calc_std(Y)/1.2
+    Pairs = 0
+    for i in Y:
+        for u in Y[i]:
+            Pairs += 1
+    mle = (1/sigma)**2 * ss_err(Y, F, S) + (np.log(2 * np.pi * sigma**2) * Pairs)
     #SSD = (float)(ss_err(Y, F, S))
     #return np.log(n)*(n-1)*h+n*(np.log(np.pi) + np.log(SSD/n)) + n
     #return np.log(n)*((n-1)*h + len(Y.keys()) * d * h) +n*(np.log(np.pi) + np.log(SSD/n)) + n 
@@ -32,7 +36,7 @@ def plot_ss():
     ss_bm = []
     for g in range(0, len(bm_files), 2):
         f = (bm_files[g].split('.')[0])
-        if ( (int) (f.split('_')[3]) < 9 and (int)(f.split('_')[3]) > 0):
+        if ( (int) (f.split('_')[3]) < 10):
             F, S =pread('results/basic_model/' + f)
             h.append(f.split('_')[3])
             ss_bm.append(ss_err(Y, F, S))
@@ -56,7 +60,7 @@ def plot_ss():
     ss_br = []
     for g in range(0, len(br_files), 2):
         f = (br_files[g].split('.')[0])
-        if ((int)(f.split('_')[3]) < 9):
+        if ((int)(f.split('_')[3]) < 10):
             F, S =pread('tdat/basic_random/' + f)
             ss_br.append(ss_err(Z, F, S))
     tr = [f for f in os.listdir('tdat/total_random/')]
@@ -64,7 +68,7 @@ def plot_ss():
     ss_tr = []
     for g in range(0, len(tr_files), 2):
         f = (tr_files[g].split('.')[0])
-        if ((int)(f.split('_')[3]) < 9):
+        if ((int)(f.split('_')[3]) < 10):
             F, S =pread('tdat/total_random/' + f)
             ss_tr.append(ss_err(Z, F, S))
     tm = [f for f in os.listdir('results/total_model/')]
@@ -72,14 +76,14 @@ def plot_ss():
     ss_tm = []
     for g in range(0, len(tm_files), 2):
         f = (tm_files[g].split('.')[0]) 
-        if ( (int) (f.split('_')[3]) < 9 and (int)(f.split('_')[3]) > 0):
+        if ( (int) (f.split('_')[3]) < 10):
             F, S =pread('results/total_model/' + f)
             ss_tm.append(ss_err(Y, F, S))
+            #print(f.split('_')[3] + "  " + str(ss_err(Y,F,S)))
     plt.figure(1)
     #plt.ylim((-100,np.nanmax(np.hstack((ss_tm,ss_tr,ss_bm,ss_br)))))
+    plt.xlim(0, np.nanmax(h))
     plt.scatter(h,ss_tm, color = 'red', label = 'Total Model')
-    print(len(h))
-    print(len(ss_tr))
     plt.scatter(h,ss_tr, color = 'blue', label = 'Total Randomized')
     plt.scatter(h,ss_bm, color = 'green', label = 'Basic Model')
     plt.scatter(h,ss_br, color = 'black', label = 'Basic Randomized')
@@ -135,10 +139,10 @@ def plot_bic():
     plt.figure(2)
     #m = np.nanmin(np.hstack((bic_tm,bic_bm,bic_br, bic_tr)))
     #plt.ylim(m * .95, m * 1.05)
-    #plt.xlim(0, np.nanmax(h))
+    plt.xlim(0, np.nanmax(h))
     plt.scatter(h,bic_tm, color = 'red', label = 'Total Model')
     #plt.scatter(h,bic_tr, color = 'blue', label = 'Total Randomized')
-    plt.scatter(h,bic_bm, color = 'green', label = 'Basic Model')
+    #plt.scatter(h,bic_bm, color = 'green', label = 'Basic Model')
     #plt.scatter(h,bic_br, color = 'black', label = 'Basic Randomized')
     plt.title('BIC vs H')
     plt.xlabel('h values')
