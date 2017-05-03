f0622906bd0c035f8b773d473aef44f9b6394955
Derek Modzelewski <dmodzel2@ugrad6.cs.jhu.edu>
2017-05-02 22:50:21 -0400

    added basic training

--- train_model.py
+++ train_model.py
@@ -80,9 +80,55 @@ def randomize_data(Y, Yt):
             d = Y[i][u].shape[0]
             Z[i][u] = np.random.randn(d,1)
             Zt[u][i] = Z[i][u]
-            
+
     return (Z, Zt)
+
+def train_basic(Y, Yt, d=10, h=5, max_iter = 1000):
+    
+    F = {}
+    S = {}
+    
+    for i in Y:
+        F[i] = np.random.randn(d, h)
+    for u in Yt:
+        S[u] = np.random.randn(h, 1)
             
+    scale = 1.0
+    dscale = 0.1
+    momentum = 0
+    SSD_old = ss_err(Y, F, S)
+
+    for ep in range(10000):
+        tscale = scale + dscale
+
+        SSD = ss_err(Y, F, S, tscale)
+        if SSD < SSD_old:
+            momentum += 0.1*dscale
+            dscale += momentum
+        else:
+            dscale = -0.1*dscale
+            momentum = 0
+
+        SSD_old = SSD
+
+    for i in F:
+        F[i] = scale * F[i]
+    for u in S:
+        S[u] = scale * S[u]
+
+    return (F,S)
+
+
+def ss_err(Y, F, S, scale = 1.):
+    err = 0.
+
+    for i in Y:
+        for u in Y[i]:
+            R = Y[i][u] - scale*mx(F[i], S[u])
+            err += sum(R**2)
+
+    return err
+
 def main():
     Y, Yt = run_pca()
     Z, Zt = randomize_data(Y, Yt)
@@ -92,6 +138,12 @@ def main():
 
     pwrite(F, S, 'total_model_10_5')
     pwrite(FZ, SZ, 'total_randomized_10_5')
+
+    F_basic, S_basic = train_basic(Y, Yt, 10, 5)
+    FZ_basic, SZ_basic = train_basic(Z, Zt, 10, 5)
+
+    pwrite(F_basic, S_basic, 'basic_model_10_5')
+    pwrite(FZ_basic, SZ_basic, 'basic_randomized_10_5')
     
 
 if __name__ == '__main__':
